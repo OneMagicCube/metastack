e39900ff5ace569b7ecfb03ce9586bbaa44b404c
yigehaor <Hyigehaor@foxmail.com>
2026-03-27 10:31:18 +0800

    fix code for --app=list

--- src/srun/opt.c
+++ src/srun/opt.c
@@ -514,6 +514,31 @@ extern int initialize_and_process_args(int argc, char **argv, int *argc_off)
 
 		/* initialize options with argv */
 		_set_options(argc, argv);
+#ifdef __METASTACK_OPT_APPTYPE_3  
+        /* Handle --app=list: print preset app list and exit.  
+         * Must be before _opt_args() which fatals if no command given. */  
+        if (opt.app && !xstrcasecmp(opt.app, "list")) {
+            slurm_ctl_conf_info_msg_app_t *app_info = NULL;  
+            if (slurm_load_app((time_t)0, &app_info) == SLURM_SUCCESS  
+                && app_info) {  
+                printf("%-20s  %s\n", "NAME", "DESCRIPTION");  
+                printf("%-20s  %s\n", "----", "-----------");  
+                for (uint32_t j = 0; j < app_info->record_count; j++) {  
+                    app_record_t *a = &app_info->app_array[j];  
+                    char *combined = NULL;  
+                    xstrfmtcat(combined, "%s-%s",  
+                               a->app_name, a->version);  
+                    printf("%-20s  %s\n", combined,  
+                           a->description ? a->description : "");  
+                    xfree(combined);  
+                }  
+                slurm_free_app_info_msg(app_info);  
+            } else {  
+                error("Unable to load app configuration");  
+            }  
+            exit(0);  
+        }  
+#endif
 		_opt_args(argc, argv, i);
 
 
@@ -579,30 +604,6 @@ extern int initialize_and_process_args(int argc, char **argv, int *argc_off)
 		if ((opt.apptype == NULL || xstrcmp(opt.apptype, "unset") != 0) && opt.argc >= 1) {
 			opt.apptype = xstrdup(opt.argv[0]);
 		}
-#endif
-#ifdef __METASTACK_OPT_APPTYPE_3  
-		/* Handle --app=list: print preset app list and exit */  
-		if (opt.app && !xstrcasecmp(opt.app, "list")) {  
-			slurm_ctl_conf_info_msg_app_t *app_info = NULL;  
-			if (slurm_load_app((time_t)0, &app_info) == SLURM_SUCCESS  
-			    && app_info) {  
-				printf("%-20s  %s\n", "NAME", "DESCRIPTION");  
-				printf("%-20s  %s\n", "----", "-----------");  
-				for (uint32_t j = 0; j < app_info->record_count; j++) {  
-					app_record_t *a = &app_info->app_array[j];  
-					char *combined = NULL;  
-					xstrfmtcat(combined, "%s-%s",  
-					           a->app_name, a->version);  
-					printf("%-20s  %s\n", combined,  
-					       a->description ? a->description : "");  
-					xfree(combined);  
-				}  
-				slurm_free_app_info_msg(app_info);  
-			} else {  
-				error("Unable to load app configuration");  
-			}  
-			exit(0);  
-		}  
 #endif
 		if (cli_filter_g_pre_submit(&opt, i)) {
 			error("cli_filter plugin terminated with error");
