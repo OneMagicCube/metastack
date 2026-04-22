985ef25e23890ebeed2ea91e4bccf7c901f78b07
OneMagicCube <1163392463@qq.com>
2026-04-22 10:21:58 +0800

    version -> versions

--- src/slurmctld/proc_req.c
+++ src/slurmctld/proc_req.c
@@ -2436,13 +2436,13 @@ static void _slurm_rpc_create_app(slurm_msg_t *msg)
 		info("%s AppName=%s Version=%s: %s",  
 		     __func__,  
 		     app_desc_ptr->app_name ? app_desc_ptr->app_name : "?",  
-		     app_desc_ptr->version ? app_desc_ptr->version : "?",  
+		     app_desc_ptr->versions ? app_desc_ptr->versions : "?",  
 		     slurm_strerror(error_code));  
 		slurm_send_rc_msg(msg, error_code);  
 	} else {  
 		info("%s complete for %s-%s %s",  
 		     __func__,  
-		     app_desc_ptr->app_name, app_desc_ptr->version,  
+		     app_desc_ptr->app_name, app_desc_ptr->versions,  
 		     TIME_STR);  
 		slurm_send_rc_msg(msg, SLURM_SUCCESS);  
 	}  
@@ -2475,13 +2475,13 @@ static void _slurm_rpc_update_app(slurm_msg_t *msg)
 		info("%s AppName=%s Version=%s: %s",  
 		     __func__,  
 		     app_desc_ptr->app_name ? app_desc_ptr->app_name : "?",  
-		     app_desc_ptr->version ? app_desc_ptr->version : "?",  
+		     app_desc_ptr->versions ? app_desc_ptr->versions : "?",  
 		     slurm_strerror(error_code));  
 		slurm_send_rc_msg(msg, error_code);  
 	} else {  
 		info("%s complete for %s-%s %s",  
 		     __func__,  
-		     app_desc_ptr->app_name, app_desc_ptr->version,  
+		     app_desc_ptr->app_name, app_desc_ptr->versions,  
 		     TIME_STR);  
 		slurm_send_rc_msg(msg, SLURM_SUCCESS);  
 	}  
